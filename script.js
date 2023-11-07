c126f32c3971d4942889f233d4e4b7f45bf1f72d
Calin <acalin041@gmail.com>
2023-11-07 16:17:45 +0200

    script.js file was updated to reflect final version of JavaScript

--- script.js
+++ script.js
@@ -1,20 +1,116 @@
 // JavaScript used to create the square divs and functionality of my sketchpad
 
 
+// let board = document.querySelector(".board");
+// board.style.gridTemplateColumns = "repeat(16,1fr)";
+// board.style.gridTemplateRows = "repeat(16,1fr)";
+
+
+// for(let i = 0; i<256; i++)
+// {
+//     let square = document.createElement('div');
+//     square.style.backgroundColor = "blue";
+//     board.insertAdjacentElement("beforeend", square);
+// }
 
 
 
-const wrapper = document.querySelector('.wrapper');
+let color = "black";
+let click = true;
 
 
 
-function createTable()
+function populateBoard(size)
 {
-    for(let i = 0; i<256; i+=1)
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+
+    squares.forEach((div) => div.remove());
+    
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    board.style.gridTemplateRows = `repeat(${size},1fr)`;
+
+    
+    let amount = size * size;
+
+    for(let i = 0; i<amount; i++)
     {
-        const createDiv = document.createElement('div');
-        wrapper.appendChild(createDiv);
+    let square = document.createElement('div');
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
     }
 }
 
-createTable();
+
+populateBoard(16);
+
+
+function changeSize(input)
+{
+    if(input >=2 && input<=100)
+    {
+    document.querySelector('.error').style.display = 'none';
+    populateBoard(input);
+    }
+    else 
+    {
+        document.querySelector('.error').style.display = 'flex';
+    }
+}
+
+
+function colorSquare(){
+    if(click)
+    {
+    if (color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else
+    {
+
+    this.style.backgroundColor = color;
+    }
+}
+}
+
+
+
+function changeColor(choice)
+{
+    color = choice;
+}
+
+
+function resetBoard()
+{
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div")
+    squares.forEach((div) => div.style.backgroundColor="white");
+}
+
+
+document.querySelector("body").addEventListener("click", (e)=>
+{
+    // click = !click;
+    // if(click)
+    // {
+    //     document.querySelector('.mode').textContent = "Mode: Coloring";
+    // }
+    // else{
+    //     document.querySelector('.mode').textContent = "Mode: Not Coloring";
+    // }
+
+    if(e.target.tagName != "BUTTON"){    
+        click = !click;
+        if(click)
+        {
+            document.querySelector('.mode').textContent = "Mode: Coloring";
+        }
+        else{
+            document.querySelector('.mode').textContent = "Mode: Not Coloring";
+        }
+    }
+
+
+});
